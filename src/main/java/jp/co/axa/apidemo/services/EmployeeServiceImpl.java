54d7e07d0fd6928f7dfe768fe2522f3e97b5fc2d
ruwan <ruwan@hsenidoutsourcing.com>
2023-03-30 13:15:16 +0900

    Add caching logic for database calls

--- src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -4,6 +4,9 @@ import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,7 +25,7 @@ public class EmployeeServiceImpl implements EmployeeService{
         List<Employee> employees = employeeRepository.findAll();
         return employees;
     }
-
+    @Cacheable(value = "employees", key = "#employeeId")
     public Employee getEmployee(Long employeeId) {
         Optional<Employee> optEmp = employeeRepository.findById(employeeId);
         return optEmp.get();
@@ -31,11 +34,11 @@ public class EmployeeServiceImpl implements EmployeeService{
     public void saveEmployee(Employee employee){
         employeeRepository.save(employee);
     }
-
+    @CacheEvict(value = "employees", key = "#employeeId")
     public void deleteEmployee(Long employeeId){
         employeeRepository.deleteById(employeeId);
     }
-
+    @CachePut(value = "employees", key = "#employee")
     public void updateEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
